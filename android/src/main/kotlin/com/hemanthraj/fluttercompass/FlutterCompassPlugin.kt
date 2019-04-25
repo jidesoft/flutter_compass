c079bc79f50081de768f8b89a73ae923b899f073
Wenjie Qiao <wqiao2@gmail.com>
2019-04-24 21:38:06 -0700

    fix android compass issue

--- android/src/main/kotlin/com/hemanthraj/fluttercompass/FlutterCompassPlugin.kt
+++ android/src/main/kotlin/com/hemanthraj/fluttercompass/FlutterCompassPlugin.kt
@@ -5,13 +5,16 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.view.WindowManager
+import android.view.Surface
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterCompassPlugin private constructor(context: Context, sensorType: Int) : EventChannel.StreamHandler {
+class FlutterCompassPlugin private constructor(registrar: Registrar, sensorType: Int) : EventChannel.StreamHandler {
   private var mAzimuth = 0.0 // degree
   private var newAzimuth = 0.0 // degree
   private var mFilter = 1f
+  private val windowManager: WindowManager
   private var sensorEventListener: SensorEventListener? = null
   private val sensorManager: SensorManager
   private var sensor: Sensor?
@@ -23,12 +26,13 @@ class FlutterCompassPlugin private constructor(context: Context, sensorType: Int
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
       val channel = EventChannel(registrar.messenger(), "hemanthraj/flutter_compass")
-      channel.setStreamHandler(FlutterCompassPlugin(registrar.context(), Sensor.TYPE_ROTATION_VECTOR))
+      channel.setStreamHandler(FlutterCompassPlugin(registrar, Sensor.TYPE_ROTATION_VECTOR))
     }
   }
 
   init {
-    sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    windowManager = registrar.activity().getWindow().getWindowManager()
+    sensorManager = registrar.context().getSystemService(Context.SENSOR_SERVICE) as SensorManager
     sensor = sensorManager.getDefaultSensor(sensorType)
   }
 
@@ -48,12 +52,53 @@ class FlutterCompassPlugin private constructor(context: Context, sensorType: Int
       override fun onSensorChanged(event: SensorEvent) {
         // calculate th rotation matrix
         SensorManager.getRotationMatrixFromVector(rMat, event.values)
-        newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 - Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2].toDouble()) + 360) % 360)
+
+        val worldAxisForDeviceAxisX: Int
+        val worldAxisForDeviceAxisY: Int
+
+        // Remap the axes as if the device screen was the instrument panel,
+        // and adjust the rotation matrix for the device orientation.
+        when (windowManager.getDefaultDisplay().getRotation()) {
+          Surface.ROTATION_0 -> {
+            worldAxisForDeviceAxisX = SensorManager.AXIS_X
+            worldAxisForDeviceAxisY = SensorManager.AXIS_Z
+            newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 - Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2].toDouble()) + 360) % 360)
+          }
+          Surface.ROTATION_90 -> {
+            worldAxisForDeviceAxisX = SensorManager.AXIS_Z
+            worldAxisForDeviceAxisY = SensorManager.AXIS_MINUS_X
+            newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 + Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[1].toDouble()) + 360 + 90) % 360)
+          }
+          Surface.ROTATION_180 -> {
+            worldAxisForDeviceAxisX = SensorManager.AXIS_MINUS_X
+            worldAxisForDeviceAxisY = SensorManager.AXIS_MINUS_Z
+            newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 + Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2].toDouble()) + 360) % 360)
+          }
+          Surface.ROTATION_270 -> {
+            worldAxisForDeviceAxisX = SensorManager.AXIS_MINUS_Z
+            worldAxisForDeviceAxisY = SensorManager.AXIS_X
+            newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 - Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[1].toDouble()) + 360 - 90) % 360)
+          }
+          else -> {
+            worldAxisForDeviceAxisX = SensorManager.AXIS_X
+            worldAxisForDeviceAxisY = SensorManager.AXIS_Z
+            newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 - Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2].toDouble()) + 360) % 360)
+          }
+        }
+
+//        val adjustedRotationMatrix = FloatArray(9)
+//        SensorManager.remapCoordinateSystem(rMat, worldAxisForDeviceAxisX,
+//                worldAxisForDeviceAxisY, adjustedRotationMatrix)
+
+//        newAzimuth = (Math.toDegrees(adjustedRotationMatrix[0].toDouble()) + 270) % 360
+
+//        newAzimuth = (((Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360) % 360 - Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2].toDouble()) + 360) % 360)
 
         //dont react to changes smaller than the filter value
         if (Math.abs(mAzimuth - newAzimuth) < mFilter) {
           return
         }
+
         mAzimuth = newAzimuth
 
         events.success(newAzimuth.toDouble())
